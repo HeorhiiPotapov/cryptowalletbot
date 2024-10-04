63edc639599d3d2f2d2fb5f54a4bd3d113a707d2
HeorhiiPotapov <bayjel13@gmail.com>
2024-10-04 11:05:38 +0300

    2

--- parser.py
+++ parser.py
@@ -35,22 +35,22 @@ def parse_address(address: str) -> dict | None:
     # setup undetected-chromedriver
     options = uc.ChromeOptions()
     user_agent = UserAgent().random
-    options.add_argument(f'--user-agent={user_agent}')
+    # options.add_argument(f'--user-agent={user_agent}')
     options.add_argument('--no-sandbox')
     options.add_argument("--headless")
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-blink-features=AutomationControlled')
 
     # Using undetected-chromedriver
-    browser = uc.Chrome(options=options, executable_path='chromedriver')
+    browser = uc.Chrome(options=options)  # , executable_path='chromedriver')
     browser.execute_cdp_cmd('Network.setExtraHTTPHeaders', {
         "headers": {
             'Host': 'dexcheck.ai',
             'Connection': 'keep-alive',
             'sec-ch-ua': '"Chromium";v="114", "Google Chrome";v="114", "Not A Brand";v="99"',
             'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+            'sec-ch-ua-platform': '"Linux"',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:114.0) Gecko/20100101 Firefox/114.0',
             'Accept': 'application/json, text/plain, */*',
             'Sec-Fetch-Site': 'same-origin',
             'Sec-Fetch-Mode': 'cors',
